ca4b69a15dd6b7da2df7c0063b69496abf7ef425
Levis <levislibra@hotmail.com>
2021-06-02 01:53:55 -0300

    Update

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -7,31 +7,20 @@ import json
 _logger = logging.getLogger(__name__)
 class FinancieraPagos360WebhookController(http.Controller):
 
-	@http.route("/financiera.pagos.360/test", type='json', auth='public', methods=['POST'], csrf=False)
-	def webhook_listener(self):
-		_logger.info('Pagos360: nuevo webhook test.')
-		data = json.loads(request.httprequest.data)
-		args = json.loads(request.httprequest.args)
-		print("print data:: ", data)
-		print("print args:: ", args)
-
 	@http.route("/financiera.pagos.360/webhook", type='json', auth='none', cors='*', csrf=False)
-	def webhook_listener(self):
+	def webhook_listener(self, **post):
 		_logger.info('Pagos360: nuevo webhook.')
-		data = json.loads(request.httprequest.data)
-		args = json.loads(request.httprequest.args)
-		print("print data:: ", data)
-		print("print args:: ", args)
-		_logger.info("///////////////////--")
+		_logger.info("jsonrequest///////////////////--")
+		_logger.info(request.jsonrequest)
+		data = request.jsonrequest
 		_logger.info(data)
-		_logger.info(args)
-		_logger.info('////////////////////--')
+		_logger.info('////////////////')
 		if 'entity_name' in data.keys():
-			print("entity_name:: ", data.get('entity_name'))
+			_logger.info(data.get('entity_name'))
 		if 'type' in data.keys():
-			print("type:: ", data.get('type'))
+			_logger.info(data.get('type'))
 		if 'entity_id' in data.keys():
-			print("entity_id:: ", data.get('entity_id'))
+			_logger.info(data.get('entity_id'))
 		webhook_type = None
 		entity_id = None
 		if 'entity_name' in data.keys():
@@ -41,18 +30,17 @@ class FinancieraPagos360WebhookController(http.Controller):
 			_id = request.env['financiera.prestamo.cuota'].sudo().search([('pagos_360_solicitud_id','=', int(entity_id))])
 			if _id and len(_id) > 0:
 				cuota_id = request.env['financiera.prestamo.cuota'].sudo().browse(int(_id[0]))
-				print("CUOTA:: ", cuota_id)
-				print("CUOTA.saldo:: ", cuota_id.saldo)
+				_logger.info("CUOTA:: ", cuota_id)
+				_logger.info("CUOTA.saldo:: ", cuota_id.saldo)
 				_logger.info('Pagos360: tipo '+data.get('type'))
 				if webhook_type == "paid":
 					# Cobrar y facturar
 					cuota_id.button_actualizar_estado()
 					_logger.info('Pagos360: nuevo pago procesado.')
-				elif webhook_type == "expired":
-					# Renovar
-					cuota_id.cuota_id.pagos_360_renovar_solicitud()
-					_logger.info('Pagos360: expiro Cupon de Pago.')
+				# elif webhook_type == "expired":
+				# 	# Renovar
+				# 	cuota_id.cuota_id.pagos_360_renovar_solicitud()
+				# 	_logger.info('Pagos360: expiro Cupon de Pago.')
 			else:
 				_logger.warning('Pagos360: No existe referencia de la cuota.')
 		return json.dumps("OK")
-
